c7da5a8644773f01af5f99066db64a54c3dc0fcf
RafaelSalles1 <48055112+RafaelSalles1@users.noreply.github.com>
2023-07-03 16:02:37 -0300

    Controllers ajustados

--- 03-task-manager/starter/db/connect.js
+++ 03-task-manager/starter/db/connect.js
@@ -1,10 +1,8 @@
 const mongoose = require('mongoose');
 
-const connectionString = 'mongodb+srv://salles:cKzV2P84ikA5dV8a@nodeprojects.rvmodwq.mongodb.net/03-TASK-MANAGER?retryWrites=true&w=majority'
-
 const connectDB = (url) => {
     return mongoose
-    .connect(connectionString, {
+    .connect(url, {
         useNewUrlParser: true,
         useCreateIndex: true,
         useFindAndModify: false,
